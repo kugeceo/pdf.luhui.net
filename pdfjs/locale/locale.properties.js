bb90af3be4d795f931f546223f7b2ae9e1fecdea
鲁虺 <37980424+kugeceo@users.noreply.github.com>
2021-09-16 20:11:27 +0800

    Add files via upload

---
+++ pdfjs/locale/locale.properties.js
@@ -0,0 +1,369 @@
+[ach]
+@import url(ach/viewer.properties.js)
+
+[af]
+@import url(af/viewer.properties.js)
+
+[ak]
+@import url(ak/viewer.properties.js)
+
+[an]
+@import url(an/viewer.properties.js)
+
+[ar]
+@import url(ar/viewer.properties.js)
+
+[as]
+@import url(as/viewer.properties.js)
+
+[ast]
+@import url(ast/viewer.properties.js)
+
+[az]
+@import url(az/viewer.properties.js)
+
+[be]
+@import url(be/viewer.properties.js)
+
+[bg]
+@import url(bg/viewer.properties.js)
+
+[bn-BD]
+@import url(bn-BD/viewer.properties.js)
+
+[bn-IN]
+@import url(bn-IN/viewer.properties.js)
+
+[br]
+@import url(br/viewer.properties.js)
+
+[brx]
+@import url(brx/viewer.properties.js)
+
+[bs]
+@import url(bs/viewer.properties.js)
+
+[ca]
+@import url(ca/viewer.properties.js)
+
+[cak]
+@import url(cak/viewer.properties.js)
+
+[crh]
+@import url(crh/viewer.properties.js)
+
+[cs]
+@import url(cs/viewer.properties.js)
+
+[csb]
+@import url(csb/viewer.properties.js)
+
+[cy]
+@import url(cy/viewer.properties.js)
+
+[da]
+@import url(da/viewer.properties.js)
+
+[de]
+@import url(de/viewer.properties.js)
+
+[el]
+@import url(el/viewer.properties.js)
+
+[en-CA]
+@import url(en-CA/viewer.properties.js)
+
+[en-GB]
+@import url(en-GB/viewer.properties.js)
+
+[en-US]
+@import url(en-US/viewer.properties.js)
+
+[en-ZA]
+@import url(en-ZA/viewer.properties.js)
+
+[eo]
+@import url(eo/viewer.properties.js)
+
+[es-AR]
+@import url(es-AR/viewer.properties.js)
+
+[es-CL]
+@import url(es-CL/viewer.properties.js)
+
+[es-ES]
+@import url(es-ES/viewer.properties.js)
+
+[es-MX]
+@import url(es-MX/viewer.properties.js)
+
+[et]
+@import url(et/viewer.properties.js)
+
+[eu]
+@import url(eu/viewer.properties.js)
+
+[fa]
+@import url(fa/viewer.properties.js)
+
+[ff]
+@import url(ff/viewer.properties.js)
+
+[fi]
+@import url(fi/viewer.properties.js)
+
+[fr]
+@import url(fr/viewer.properties.js)
+
+[fy-NL]
+@import url(fy-NL/viewer.properties.js)
+
+[ga-IE]
+@import url(ga-IE/viewer.properties.js)
+
+[gd]
+@import url(gd/viewer.properties.js)
+
+[gl]
+@import url(gl/viewer.properties.js)
+
+[gn]
+@import url(gn/viewer.properties.js)
+
+[gu-IN]
+@import url(gu-IN/viewer.properties.js)
+
+[he]
+@import url(he/viewer.properties.js)
+
+[hi-IN]
+@import url(hi-IN/viewer.properties.js)
+
+[hr]
+@import url(hr/viewer.properties.js)
+
+[hsb]
+@import url(hsb/viewer.properties.js)
+
+[hto]
+@import url(hto/viewer.properties.js)
+
+[hu]
+@import url(hu/viewer.properties.js)
+
+[hy-AM]
+@import url(hy-AM/viewer.properties.js)
+
+[ia]
+@import url(ia/viewer.properties.js)
+
+[id]
+@import url(id/viewer.properties.js)
+
+[is]
+@import url(is/viewer.properties.js)
+
+[it]
+@import url(it/viewer.properties.js)
+
+[ja]
+@import url(ja/viewer.properties.js)
+
+[ka]
+@import url(ka/viewer.properties.js)
+
+[kab]
+@import url(kab/viewer.properties.js)
+
+[kk]
+@import url(kk/viewer.properties.js)
+
+[km]
+@import url(km/viewer.properties.js)
+
+[kn]
+@import url(kn/viewer.properties.js)
+
+[ko]
+@import url(ko/viewer.properties.js)
+
+[kok]
+@import url(kok/viewer.properties.js)
+
+[ks]
+@import url(ks/viewer.properties.js)
+
+[ku]
+@import url(ku/viewer.properties.js)
+
+[lg]
+@import url(lg/viewer.properties.js)
+
+[lij]
+@import url(lij/viewer.properties.js)
+
+[lo]
+@import url(lo/viewer.properties.js)
+
+[lt]
+@import url(lt/viewer.properties.js)
+
+[ltg]
+@import url(ltg/viewer.properties.js)
+
+[lv]
+@import url(lv/viewer.properties.js)
+
+[mai]
+@import url(mai/viewer.properties.js)
+
+[meh]
+@import url(meh/viewer.properties.js)
+
+[mk]
+@import url(mk/viewer.properties.js)
+
+[ml]
+@import url(ml/viewer.properties.js)
+
+[mn]
+@import url(mn/viewer.properties.js)
+
+[mr]
+@import url(mr/viewer.properties.js)
+
+[ms]
+@import url(ms/viewer.properties.js)
+
+[my]
+@import url(my/viewer.properties.js)
+
+[nb-NO]
+@import url(nb-NO/viewer.properties.js)
+
+[ne-NP]
+@import url(ne-NP/viewer.properties.js)
+
+[nl]
+@import url(nl/viewer.properties.js)
+
+[nn-NO]
+@import url(nn-NO/viewer.properties.js)
+
+[nso]
+@import url(nso/viewer.properties.js)
+
+[oc]
+@import url(oc/viewer.properties.js)
+
+[or]
+@import url(or/viewer.properties.js)
+
+[pa-IN]
+@import url(pa-IN/viewer.properties.js)
+
+[pl]
+@import url(pl/viewer.properties.js)
+
+[pt-BR]
+@import url(pt-BR/viewer.properties.js)
+
+[pt-PT]
+@import url(pt-PT/viewer.properties.js)
+
+[rm]
+@import url(rm/viewer.properties.js)
+
+[ro]
+@import url(ro/viewer.properties.js)
+
+[ru]
+@import url(ru/viewer.properties.js)
+
+[rw]
+@import url(rw/viewer.properties.js)
+
+[sah]
+@import url(sah/viewer.properties.js)
+
+[sat]
+@import url(sat/viewer.properties.js)
+
+[si]
+@import url(si/viewer.properties.js)
+
+[sk]
+@import url(sk/viewer.properties.js)
+
+[sl]
+@import url(sl/viewer.properties.js)
+
+[son]
+@import url(son/viewer.properties.js)
+
+[sq]
+@import url(sq/viewer.properties.js)
+
+[sr]
+@import url(sr/viewer.properties.js)
+
+[sv-SE]
+@import url(sv-SE/viewer.properties.js)
+
+[sw]
+@import url(sw/viewer.properties.js)
+
+[ta]
+@import url(ta/viewer.properties.js)
+
+[ta-LK]
+@import url(ta-LK/viewer.properties.js)
+
+[te]
+@import url(te/viewer.properties.js)
+
+[th]
+@import url(th/viewer.properties.js)
+
+[tl]
+@import url(tl/viewer.properties.js)
+
+[tn]
+@import url(tn/viewer.properties.js)
+
+[tr]
+@import url(tr/viewer.properties.js)
+
+[tsz]
+@import url(tsz/viewer.properties.js)
+
+[uk]
+@import url(uk/viewer.properties.js)
+
+[ur]
+@import url(ur/viewer.properties.js)
+
+[uz]
+@import url(uz/viewer.properties.js)
+
+[vi]
+@import url(vi/viewer.properties.js)
+
+[wo]
+@import url(wo/viewer.properties.js)
+
+[xh]
+@import url(xh/viewer.properties.js)
+
+[zam]
+@import url(zam/viewer.properties.js)
+
+[zh-CN]
+@import url(zh-CN/viewer.properties.js)
+
+[zh-TW]
+@import url(zh-TW/viewer.properties.js)
+
+[zu]
+@import url(zu/viewer.properties.js)
+
